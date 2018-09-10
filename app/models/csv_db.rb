5156287527098e62c7d16ea47d7f295ce7770644
Daniel Osorio <danielosorio83@gmail.com>
2018-09-10 17:12:23 -0500

    Update csv_db.rb

--- app/models/csv_db.rb
+++ app/models/csv_db.rb
@@ -3,7 +3,7 @@ class CsvDb
   class << self
     def convert_save(target_model, csv_data, current_user, &block)
       csv_file = csv_data.read
-      line_count = %x(wc -l #{filename}).to_i
+      line_count = %x(wc -l #{csv_data.original_filename}).to_i
       CSV.parse(csv_file, headers: true, header_converters: :symbol).each_with_index do |row, line_num|
         data = row.to_hash
         is_last_row = (line_num == line_count)
